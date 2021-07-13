72c410ded0e3267deb4d6ef0b2ed366fb19cb971
sukhbirss <s.sukhbirsohal@gmail.com>
2021-07-13 23:18:22 +0530

    done 4

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,36 +1,38 @@
 import styles from './home.module.css';
-import nexgen from '../../Api/nexgenApi';
 import { useEffect,useState } from 'react'
 import { DATAA } from '../../data'
+import searchIcon from './search.svg'
+
+//components
 import Table from '../../components/table/Table'
 import MyForm from '../../components/myForm/MyForm'
-import searchIcon from './search.svg'
+
+//this is to get data from the api
+import nexgen from '../../Api/nexgenApi';
 
 function Home() {
 	const [data, setData] = useState(JSON.parse(localStorage.getItem("data")))
+	const [searchData, setSearchData] = useState(JSON.parse(localStorage.getItem("data")))
+
 	useEffect(() => {
-	
-	if(JSON.parse(localStorage.getItem("data"))){
-		setData(JSON.parse(localStorage.getItem("data")))
-	}
-	else{
-		nexgen.get("/deb1fcca441912d3541c").then((res) =>{
- 	    setData(res.data.data)})
-		// setData(DATAA)
-	}
+		if(!JSON.parse(localStorage.getItem("data"))){
+				nexgen.get("/deb1fcca441912d3541c").then((res) =>{
+		 	    setData(res.data.data)
+		 	    setSearchData(res.data.data)
+		 	})
+			}
 	}, [])
 
-	const searchFunc = (value) =>{
-	     let obj = data.filter((o) => o.employee_name.toLowerCase().includes(value));
+	const searchFunc = (event) =>{
+	     let obj = searchData.filter((o) => o.employee_name.toLowerCase().includes(event.target.value));
 	     if(obj){ setData(obj) }
-	     if(value === ""){ setData(DATAA) }
 	}
 
   return (
     <div className={styles.wrapper}>
     	<MyForm setData={setData} data={data} />
     	<div>
-	    	<input  onChange={(e)=> searchFunc(e.target.value)} className={styles.search} placeholder="Search for employee_name in here"/>
+	    	<input  onChange={(e)=> searchFunc(e)} className={styles.search} placeholder="Search for employee_name in here"/>
 	    	<Table data={data} />
 	    </div>
     </div>
